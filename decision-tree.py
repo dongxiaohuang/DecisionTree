7b31a4098183ac79797df2a1c2b44c0cd3ccc9d3
Yufeng Zhang <yz21916@point36.doc.ic.ac.uk>
2018-02-01 12:14:59 +0000

    Add some comments.

--- decision-tree.py
+++ decision-tree.py
@@ -2,35 +2,68 @@ from scipy.io import loadmat
 import numpy as np
 from math import log
 
+"""
+# function to load data from a matlab file
+# the data contains a tuple of features and labels
+#
+# @param filename   - the path of the file
+# @return           - data of features, and data of labels
+"""
 def load_data(filename):
-    """Returns a tuple of features and labels."""
     data = loadmat(filename, squeeze_me = True)
+
     return data['x'], data['y']
 
-emotions = {'anger': 1,
-            'disgust': 2,
-            'fear': 3,
-            'happiness': 4,
-            'sadness': 5,
-            'surprise': 6}
+#map emotions to labels
+emotions = {'anger'     : 1,
+            'disgust'   : 2,
+            'fear'      : 3,
+            'happiness' : 4,
+            'sadness'   : 5,
+            'surprise'  : 6}
 
+"""
+# function to replace label with 1 or 0 for emotion present or absent respectively
+#
+# @param labels     - a list of labels from 1 to 6
+# @param emotion    - the emotion to classify
+# @return           - a list of labels 1 or 0 for emotion present of absent
+"""
 def map_label (labels, emotion):
-    """Replace label with 1 or 0 if emotion is present or absent."""
     value = emotions.get(emotion, -1)
+
     return np.array([1 if lab == value else 0 for lab in labels])
 
+"""
+# function to compute the majority value of a list
+#
+# @param binary_targets - a list with values 0 and 1
+# @return               - 0 if the number of 0 is more than that of 1
+                          1 if the number of 1 is more than that of 0
+"""
 def majority_value(binary_targets):
     count = 0
     for item in binary_targets:
         count += 1 if item == 1 else -1
+
     return 1 if count > 0 else 0
 
+"""
+# function to compute the entropy
+#
+# @param pos    - number of positive examples
+# @param neg    - number of negative examples
+# @return       - the entropy
+"""
 def entropy (pos, neg):
     if pos == 0 or neg == 0: return 0
     p = 1.0 * pos / (pos + neg)
     n = 1.0 * neg / (pos + neg)
+
     return - p * log(p, 2) - n * log(n, 2)
 
+"""
+"""
 def remainder (attribute, sum_pn, examples, binary_targets):
     [p0, p1, n0, n1] = np.zeros(4, float)
     if sum_pn == 0:
@@ -46,8 +79,11 @@ def remainder (attribute, sum_pn, examples, binary_targets):
                 n1 += 1
             else:
                 n0 += 1
-    return (p0+n0)/sum_pn * entropy(p0, n0) + (p1+n1)/sum_pn * entropy(p1, n1)
+   
+   return (p0+n0)/sum_pn * entropy(p0, n0) + (p1+n1)/sum_pn * entropy(p1, n1)
 
+"""
+"""
 def gain (attribute, examples, binary_targets):
     [p, n] = np.zeros(2, float)
     for item in binary_targets:
@@ -55,16 +91,22 @@ def gain (attribute, examples, binary_targets):
             p += 1
         else:
             n += 1
-    return entropy(p, n) - remainder(attribute, p+n, examples, binary_targets)
+   
+   return entropy(p, n) - remainder(attribute, p+n, examples, binary_targets)
 
+"""
+"""
 def choose_best_attribute(examples, attributes, binary_targets):
     [best_gain, best] = np.zeros(2, int)
     for attribute in attributes:
         temp_gain = gain(attribute, examples, binary_targets)
         if temp_gain > best_gain:
             [best_gain, best] = [temp_gain, attribute]
-    return best
+   
+   return best
 
+"""
+"""
 class Node:
 
     def __init__ (self, kids, op = None, label = None):
@@ -75,6 +117,8 @@ class Node:
     def add_kid (self, kid):
         self.kids.append(kid)
 
+"""
+"""
 def find_elements(examples, binary_targets, attribute, value):
     index = []
     binary_targets_i = np.array([], dtype=int)
@@ -82,8 +126,11 @@ def find_elements(examples, binary_targets, attribute, value):
         if examples[i, attribute] == value:
             index.append(i)
             binary_targets_i = np.append(binary_targets_i, binary_targets[i])
-    return [examples[index, :], binary_targets_i]
+   
+   return [examples[index, :], binary_targets_i]
 
+"""
+"""
 def decision_tree_learning(examples, attributes, binary_targets):
     if len(set(binary_targets)) == 1:
 #       print "leaf"
@@ -106,13 +153,19 @@ def decision_tree_learning(examples, attributes, binary_targets):
                 new_attribute.remove(best_attribute)
                 subtree = decision_tree_learning(examples_i, new_attribute, binary_targets_i)
                 tree.add_kid(subtree)
-        return tree
+   
+   return tree
 
+"""
+"""
 def test_trees(T, features):
     while T.op != None:
         T = T.kids[features[T.op]]
-    return T.label
+   
+   return T.label
 
+"""
+"""
 def classify_emotion(examples):
     result = [test_trees(anger_decision_tree, examples),
               test_trees(disgust_decision_tree, examples),
@@ -130,40 +183,21 @@ X, y = load_data("cleandata_students.mat")
 nx, ny = load_data("noisydata_students.mat")
 attributes = list(xrange(45))
 
-<<<<<<< HEAD
-anger_targets = map_label(data['y'], "anger")
-disgust_targets = map_label(data['y'], "disgust")
-fear_targets = map_label(data['y'], "fear")
-happiness_targets = map_label(data['y'], "happiness")
-sadness_targets = map_label(data['y'], "sadness")
-surprise_targets = map_label(data['y'], "surprise")
-
-anger_decision_tree = decision_tree_learning(data['x'], attributes, anger_targets)
-disgust_decision_tree = decision_tree_learning(data['x'], attributes, disgust_targets)
-fear_decision_tree = decision_tree_learning(data['x'], attributes, fear_targets)
-happiness_decision_tree = decision_tree_learning(data['x'], attributes, happiness_targets)
-sadness_decision_tree = decision_tree_learning(data['x'], attributes, sadness_targets)
-surprise_decision_tree = decision_tree_learning(data['x'], attributes, surprise_targets)
-
-#for i in data['x']:
-print test_trees(sadness_decision_tree, data['x'][1])
-=======
-anger_targets     = map_label(y, "anger")
-disgust_targets   = map_label(y, "disgust")
-fear_targets      = map_label(y, "fear")
-happiness_targets = map_label(y, "happiness")
-sadness_targets   = map_label(y, "sadness")
-surprise_targets  = map_label(y, "surprise")
+anger_targets       = map_label(y, "anger")
+disgust_targets     = map_label(y, "disgust")
+fear_targets        = map_label(y, "fear")
+happiness_targets   = map_label(y, "happiness")
+sadness_targets     = map_label(y, "sadness")
+surprise_targets    = map_label(y, "surprise")
 
 test = map_label(ny,"sadness")
 
-anger_decision_tree = decision_tree_learning(X, attributes, anger_targets)
-disgust_decision_tree = decision_tree_learning(X, attributes, disgust_targets)
-fear_decision_tree = decision_tree_learning(X, attributes, fear_targets)
+anger_decision_tree     = decision_tree_learning(X, attributes, anger_targets)
+disgust_decision_tree   = decision_tree_learning(X, attributes, disgust_targets)
+fear_decision_tree      = decision_tree_learning(X, attributes, fear_targets)
 happiness_decision_tree = decision_tree_learning(X, attributes, happiness_targets)
-sadness_decision_tree = decision_tree_learning(X, attributes, sadness_targets)
-surprise_decision_tree = decision_tree_learning(X, attributes, surprise_targets)
-
+sadness_decision_tree   = decision_tree_learning(X, attributes, sadness_targets)
+surprise_decision_tree  = decision_tree_learning(X, attributes, surprise_targets)
 
 # calculate the precision rate
 predictx =[]
@@ -177,4 +211,3 @@ for i in nx:
 diff = (predictx - ny)
 print float(sum(x == 0 for x in diff))/len(diff)
 # print classify_emotion(nx[342]),ny[342]
->>>>>>> origin/HEAD
